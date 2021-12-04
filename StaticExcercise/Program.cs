045801a8cd65dc48eea9815fd6e05cc7e7609a30
thirtyThreeS <thaumasios@outlook.com>
2021-12-04 12:22:34 -0500

    initial commit

---
+++ StaticExcercise/Program.cs
@@ -0,0 +1,19 @@
+﻿//Exercise 1: Create a new console app called StaticExercise.Now create a static class called TempConverter.
+//The class should have at least 2 methods, one called FahrenheitToCelsius that will require a double as a parameter 
+//and return a double, the other CelsiusToFahrenheit which will also require a double as a parameter and return a double. 
+//Fill out these methods and call them in your Program.cs file. Make sure they’re accurate! 
+
+namespace StaticExercise
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("84 degrees C = " + TempConverter.CelsiusToFahrenheit(84) + " F");
+            Console.WriteLine("180 degrees F = " + TempConverter.FahrenheitToCelsius(180) + " C");
+
+            Console.ReadLine();
+            Console.WriteLine("Al.. most.. cri.. ti.. cal.. GP... U... temps. Boo.. sting... fans... to ...//// 100 .... %");
+        }
+    }
+}
---
+++ StaticExcercise/TempConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public class TempConverter
+    {
+        public static double FahrenheitToCelsius(double f) => (5.0 / 9.0) * (f - 32);
+        public static double CelsiusToFahrenheit(double c) => ((9.0 / 5.0) * c) + 32;
+        
+
+        
+    }
+}
